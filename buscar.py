fe0069e4f26494ca877d74b3df0510f38b3de24c
Due204 <due204@gmail.com>
2021-07-07 05:47:14 -0300

    07/07/2021 05:45

--- buscar.py
+++ buscar.py
@@ -1,9 +1,12 @@
 from tkinter import Label
 from tkinter import Entry
+from tkinter import Frame
 from tkinter import IntVar
 from tkinter import Button
 from tkinter import Listbox
 from tkinter import Toplevel
+from tkinter import Scrollbar
+from tkinter import StringVar
 from tkinter import Radiobutton
 from base import BaseDatos
 
@@ -13,8 +16,10 @@ def busqueda():
     toor = Toplevel()  # Creamos una ventana
     toor.title("Busqueda")  # Titulo de la ventana
     toor.geometry("425x420")  # Tamaño de la ventana
+    toor.resizable(False, False)
     parame = []
     numoer = IntVar()
+    totales = StringVar()
 
     def valor_boton(*argus):
         nume = numoer.get()
@@ -27,6 +32,7 @@ def busqueda():
             else:
                 lisbo.delete(0, "end")
                 lisbo.insert(0, "Ingrese un numero de orden")
+                totales.set("Encontrados: 0")
         elif nume == 2:
             lisbo.delete(0, "end")
             busqueda_g("apellido")
@@ -38,6 +44,7 @@ def busqueda():
             else:
                 lisbo.delete(0, "end")
                 lisbo.insert(0, "Ingrese un numero de telefono")
+                totales.set("Encontrados: 0")
         elif nume == 4:
             lisbo.delete(0, "end")
             busqueda_g("tipo")
@@ -47,6 +54,7 @@ def busqueda():
         else:
             lisbo.delete(0, "end")
             lisbo.insert(0, "Seleccione una opcion")
+            totales.set("Encontrados: 0")
 
     def busqueda_g(parametro):
         lisbo.delete(0, "end")
@@ -105,6 +113,8 @@ def busqueda():
                     )
                     parame.append("N° de Orden: " + str(i))
 
+            totales.set("Encontrados: " + str(len(parame) // 12))
+
             for item in parame:  # Insertamos los items en un Listbox
                 lisbo.insert(0, item)
 
@@ -121,6 +131,7 @@ def busqueda():
             else:
                 lisbo.insert(0, "Orden de reparacion")
                 lisbo.insert(1, " no encotrada.")
+                totales.set("Encontrados: 0")
         else:
             lisbo.delete(0, "end")
             lisbo.insert(0, "Ingrese un numero")
@@ -166,25 +177,42 @@ def busqueda():
         for item in parame:  # Insertamos los items en un Listbox
             lisbo.insert(0, item)
 
+        totales.set("Encontrados: 1")
+
     # posicionamiento de los widget de tkinter
-    texto = Label(toor, text="Buscar por:")
-    texto.place(x=10, y=0)
-    busq1 = Radiobutton(toor, text="Orden", value=1, variable=numoer)
-    busq1.place(x=0, y=25)
-    busq2 = Radiobutton(toor, text="Apellido", value=2, variable=numoer)
-    busq2.place(x=70, y=25)
-    busq3 = Radiobutton(toor, text="Telefono", value=3, variable=numoer)
-    busq3.place(x=160, y=25)
-    busq4 = Radiobutton(toor, text="Tipo", value=4, variable=numoer)
-    busq4.place(x=250, y=25)
-    busq5 = Radiobutton(toor, text="Marca", value=5, variable=numoer)
-    busq5.place(x=310, y=25)
-    entra = Entry(toor, width=10, bd=3)
-    entra.place(x=150, y=50)
-    boton = Button(toor, text="Aceptar", command=valor_boton)
-    boton.place(x=155, y=80)
+    frame1 = Frame(toor)
+    frame1.place(x=0, y=0)
+    frame2 = Frame(toor)
+    frame2.place(x=0, y=120)
+    texto = Label(frame1, text="Buscar por:")
+    texto.grid(row=0, column=2)
+    busq1 = Radiobutton(frame1, text="Orden", value=1, variable=numoer)
+    busq1.grid(row=1, column=0)
+    busq2 = Radiobutton(frame1, text="Apellido", value=2, variable=numoer)
+    busq2.grid(row=1, column=1)
+    busq3 = Radiobutton(frame1, text="Telefono", value=3, variable=numoer)
+    busq3.grid(row=1, column=2)
+    busq4 = Radiobutton(frame1, text="Tipo", value=4, variable=numoer)
+    busq4.grid(row=1, column=3)
+    busq5 = Radiobutton(frame1, text="Marca", value=5, variable=numoer)
+    busq5.grid(row=1, column=4)
+    entra = Entry(frame1, width=10, bd=3)
+    entra.grid(row=2, column=1)
+    boton = Button(frame1, text="Aceptar", command=valor_boton)
+    boton.grid(row=2, column=2)
     boton.bind("<Return>", valor_boton)  # Esto es para el enter
-    lisbo = Listbox(toor, borderwidth=5, width=50, height=15)
-    lisbo.place(x=5, y=120)
+    result = Label(frame1, textvariable=totales)
+    result.grid(row=3, column=1)
+    tree_scroll = Scrollbar(frame2)
+    tree_scroll.pack(side="right", fill="y")
+    lisbo = Listbox(
+        frame2,
+        borderwidth=5,
+        width=50,
+        height=15,
+        yscrollcommand=tree_scroll.set,
+    )
+    lisbo.pack()
+    tree_scroll.config(command=lisbo.yview)
 
     toor.mainloop()
